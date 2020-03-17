70be403fc776864b00284eb0e214ebf47e495282
Hotanmes <44225152+Hotanmes@users.noreply.github.com>
2020-03-17 10:52:29 +0800

    Update 1048.cpp

--- PAT_advanced/1048.cpp
+++ PAT_advanced/1048.cpp
@@ -5,26 +5,26 @@ using namespace std;
 int main()
 {
 	int n, m, value;
+	vector<int> v(1001);//为什么数组大小取五百多会发生段错误？
 	cin >> n >> m;
-	vector<int> v(501);
 	for (int i = 0; i < n; ++i)
 	{
 		scanf("%d", &value);
 		++v[value];//下标代表面值（不超过500），值代表该面值出现的次数
 	}
-	for (int i = 0; i < 501; ++i)//遍历每个面值
+	for (int i = 0; i < 1001; ++i)//遍历每个面值
 	{
 		if (v[i])
 		{
 			--v[i];//标志对该面值进行处理，处理完再加回来
 			if (m - i > 0 && v[m - i])//先遍历到i，可以保证i<=m-i
 			{
-				printf("%d %d\n", i, m - i);
+				printf("%d %d", i, m - i);
 				return 0;
 			}
 			++v[i];
 		}
 	}
-	printf("No Solution\n");//注意位置
+	printf("No Solution");//注意位置
 	return 0;
 }
